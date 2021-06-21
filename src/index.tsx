b3d8ca7d31e006943f873be660fead79a11d2993
Thomas Maier (ext.) <Thomas.Maier@juniq-it.de>
2021-06-21 08:49:14 +0200

    refactor to use useBreakpoint hook in app index

--- src/index.tsx
+++ src/index.tsx
@@ -1,75 +1,70 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
-import { Grommet, ResponsiveContext } from 'grommet'
+import {Grommet} from 'grommet'
 import * as Sentry from '@sentry/browser'
 
-// Styles:
+// Styles
 import './styles/colors.css'
 import './styles/global.css'
 import './styles/effects.css'
 import './styles/components.css'
 import theme from './styles/theme'
 
-// Pages:
+// Hooks
+import {useBreakpoint} from "./hooks/useBreakpoint";
+
+// Pages
 import Home from './pages/home/'
 import Training from './pages/training'
 import Opinions from './pages/opinions'
 import Gallery from './pages/gallery'
 import Position from './pages/position'
 
-// Components:
+// Components
 import Navigation from './components/navigation/'
 import Footer from './components/footer/'
 import Spacer from './components/spacer'
 
 // Sentry Init
-Sentry.init({ dsn: process.env.REACT_APP_SENTRY_URL })
+Sentry.init({dsn: process.env.REACT_APP_SENTRY_URL})
 
-// Consts:
+// Const
 const NAVIGATION = window.innerHeight / 3
 const TOLERANCE = 25
 
 // ===============================================
 const App = () => {
-  const [scroll, setScroll] = useState<boolean>(false)
+    const {isMobile} = useBreakpoint();
 
-  return (
-    <Grommet
-      id="grommet"
-      theme={theme}
-      full
-      onScroll={event => {
-        //@ts-ignore
-        if (event.target.id === 'grommet') {
-          //@ts-ignore
-          if (event.target.scrollTop > NAVIGATION + TOLERANCE) setScroll(true)
-          //@ts-ignore
-          else if (event.target.scrollTop < NAVIGATION - TOLERANCE) setScroll(false)
-        }
-      }}
-    >
-      <ResponsiveContext.Consumer>
-        {size => {
-          const isMobile = size.includes('small')
+    const [scroll, setScroll] = useState<boolean>(false)
 
-          return (
-            <React.Fragment>
-              <Navigation scrolled={scroll} />
-              <Home />
-              <Training />
-              <Opinions />
-              <Gallery isMobile={isMobile} />
-              <Position />
-              <Footer />
-              {isMobile && <Spacer height="8em" />}
-            </React.Fragment>
-          )
-        }}
-      </ResponsiveContext.Consumer>
-    </Grommet>
-  )
+    return (
+        <Grommet
+            id="grommet"
+            theme={theme}
+            full
+            onScroll={({target}) => {
+                const {id, scrollTop} = (target as HTMLDivElement);
+
+                if (id === 'grommet') {
+                    if (scrollTop > NAVIGATION + TOLERANCE) setScroll(true)
+                    else if (scrollTop < NAVIGATION - TOLERANCE) setScroll(false)
+                }
+            }}
+        >
+            <Navigation scrolled={scroll}/>
+            <Home/>
+            <Training/>
+            <Opinions/>
+            <Gallery isMobile={isMobile}/>
+            <Position/>
+            <Footer/>
+            {isMobile && <Spacer height="8em"/>}
+        </Grommet>
+    )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// ===============================================
+ReactDOM.render(<App/>, document.getElementById('root'))
 serviceWorker.unregister()
